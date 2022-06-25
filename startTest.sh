3fb7c30988d673579b744a8b4f787ae204712cc3
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-06-25 11:41:10 +0800

    修正说明

--- startTest.sh
+++ startTest.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+# 压测机数量
 j=20
+
+# 上传测试工具
 for ((i=1; i<=j; i++))
 do
   x=$[(i-1) * 50000]
@@ -8,5 +12,11 @@ do
   scp config.toml.bak test${i}:~/config.toml
   scp wfcstress test${i}:~/
   rm -rf scp config.toml.bak
+done
+
+#同时开始
+for ((i=1; i<=j; i++))
+do
+  x=$[(i-1) * 50000]
   ssh -o ServerAliveInterval=10 test${i} "nohup ./wfcstress > console.log 2>&1 &"
 done
